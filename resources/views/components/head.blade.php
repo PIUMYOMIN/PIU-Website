021d20850681d78b1c37b4fade32b2ae678fec40
Myo Min Thu <myominthu819@gmail.com>
2024-01-11 09:02:37 +0630

    user register updated

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Education master is one of the best educational html template, it's suitable for all education websites like university,college,school,online education,tution center,distance education,computer education">
     <meta name="keyword" content="education html template, university template, college template, school template, online education template, tution center template">
     <!-- FAV ICON(BROWSER TAB ICON) -->
-    <link rel="shortcut icon" href="images/fav.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/fav.ico') }}" type="image/x-icon">
     <!-- GOOGLE FONT -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CJosefin+Sans:600,700" rel="stylesheet">
     <!-- FONTAWESOME ICONS -->
