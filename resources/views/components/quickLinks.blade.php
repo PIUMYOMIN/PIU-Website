f267a5cf60f338d51f5dddb3242fb3700c0ac36b
Myo Min Thu <myominthu819@gmail.com>
2024-01-12 08:25:57 +0630

    quick links updated

--- resources/views/components/quickLinks.blade.php
+++ resources/views/components/quickLinks.blade.php
@@ -5,16 +5,16 @@
                 <div class="wed-hom-ser">
                     <ul>
                         <li>
-                            <a href="awards.html" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic1.png" alt=""> Academy</a>
+                            <a href="/about" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic1.png" alt=""> About</a>
                         </li>
                         <li>
-                            <a href="admission.html" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic2.png" alt=""> Admission</a>
+                            <a href="/piu/admission/application-form" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic2.png" alt=""> Admission</a>
                         </li>
                         <li>
-                            <a href="all-courses.html" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic4.png" alt=""> Courses</a>
+                            <a href="/courses" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic4.png" alt=""> Courses</a>
                         </li>
                         <li>
-                            <a href="seminar.html" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic3.png" alt=""> Seminar</a>
+                            <a href="/seminars" class="waves-effect waves-light btn-large wed-pop-ser-btn"><img src="images/icon/h-ic3.png" alt=""> Seminar</a>
                         </li>
                     </ul>
                 </div>
