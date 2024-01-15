17f9cc3e9ae918a31be76a5663914337c48b9086
Myo Min Thu <myominthu819@gmail.com>
2024-01-15 15:42:23 +0630

    social links updated

--- resources/views/components/socialShare.blade.php
+++ resources/views/components/socialShare.blade.php
@@ -3,12 +3,10 @@
         <div class="icon-float">
             <ul>
                 <li><a href="#" class="sh">1k <br> Share</a> </li>
-                <li><a href="#" class="fb1"><i class="fa fa-facebook" aria-hidden="true"></i></a> </li>
-                <li><a href="#" class="gp1"><i class="fa fa-google-plus" aria-hidden="true"></i></a> </li>
+                <li><a href="https://www.facebook.com/PhaungDawOoInternationalUniveristy" target="_blank" class="fb1"><i class="fa fa-facebook" aria-hidden="true"></i></a> </li>
                 <li><a href="#" class="tw1"><i class="fa fa-twitter" aria-hidden="true"></i></a> </li>
-                <li><a href="#" class="li1"><i class="fa fa-linkedin" aria-hidden="true"></i></a> </li>
-                <li><a href="#" class="wa1"><i class="fa fa-whatsapp" aria-hidden="true"></i></a> </li>
-                <li><a href="#" class="sh1"><i class="fa fa-envelope-o" aria-hidden="true"></i></a> </li>
+                <li><a href="tel:+09-793200074" class="wa1"><i class="fa fa-phone" aria-hidden="true"></i></a> </li>
+                <li><a href="mailto:piu.edu2014@gmail.com" class="sh1"><i class="fa fa-envelope-o" aria-hidden="true"></i></a> </li>
             </ul>
         </div>
     </section>
