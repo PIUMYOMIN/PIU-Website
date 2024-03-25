f9f097c5ee73c17376000508bdd0e4e8d1d571dc
Myo Min Thu <myominthu819@gmail.com>
2024-03-25 12:58:02 +0630

    assignment edit page updated

--- resources/views/admin/assignments/edit.blade.php
+++ resources/views/admin/assignments/edit.blade.php
@@ -1,9 +1,10 @@
+{{-- @dd($assignment) --}}
 <x-admin_layout>
     <div class="sb2-2-2">
         <ul>
-            <li><a href="index-2.html"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+            <li><a href="/admin"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
             </li>
-            <li class="active-bre"><a href="#"> Add New Assignment</a>
+            <li class="active-bre"><a href="#"> Edit Assignment</a>
             </li>
             <li class="page-back"><a href="index-2.html"><i class="fa fa-backward" aria-hidden="true"></i> Back</a>
             </li>
@@ -16,7 +17,7 @@
             <div class="col-md-12">
                 <div class="box-inn-sp admin-form">
                     <div class="inn-title">
-                        <h4>Add Assignment</h4>
+                        <h4>Edit Assignment</h4>
                         <p>Here you can edit your website basic details URL, Phone, Email, Address, User and password
                             and more</p>
                     </div>
@@ -27,8 +28,7 @@
                             @method('PATCH')
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input type="text" value="{{ $assignment->name }}" name="name" class="validate" required>
-                                    <label class="">Assignment Name</label>
+                                    <input type="text" value="{{ $assignment->name }}" name="name" class="validate">
                                 </div>
                                 @error('name')
                                     <p class="text-danger">{{ $message }}</p>
@@ -47,7 +47,7 @@
                                 <div class="input-field col s6">
                                     <select class="input-field col s12" name="course_id">
                                         @foreach ($courses as $course)
-                                            <option {{ $course->id == old('course_id') ? 'selected' : '' }} value="{{ $course->id }}">{{ $course->title }}</option>
+                                            <option {{ $assignment->course_id == $course->id ? 'selected' : '' }} value="{{ $course->id }}">{{ $course->title }}</option>
                                         @endforeach
                                     </select>
                                     @error('course_id')
