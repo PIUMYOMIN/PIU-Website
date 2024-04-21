a6e9a991772ff03a0c32b69fe6d9ea14f681bfdb
Myo Min Thu <myominthu819@gmail.com>
2024-04-21 13:53:52 +0630

    user login api updated

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -66,26 +66,32 @@ class UserController extends Controller
     }
 
     public function apiLogin(Request $request)
-    {
-        $input = $request->all();
+{
+    $input = $request->all();
 
-        $validation = Validator::make($input,[
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+    $validation = Validator::make($input, [
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
 
-        if($validation->fails()){
-            return response()->json(['error' => $validation->errors()],422);
-        }
-
-        if(Auth::attempt(['email' => $input['email'], 'password' => $input['password']])){
-            $user = Auth::user();
-
-            $token = $user->createToken('MyToken')->accessToken;
-
-            return response()->json(['token' => $token]);
-        }
+    if ($validation->fails()) {
+        return response()->json(['error' => $validation->errors()], 422);
     }
+
+    if (Auth::attempt(['email' => $input['email'], 'password' => $input['password']])) {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $token = $user->createToken('MyToken')->accessToken;
+
+        return response()->json(['token' => $token]);
+    } else {
+        return response()->json(['error' => 'Unauthenticated'], 401);
+    }
+}
 
     public function userDetails()
     {
