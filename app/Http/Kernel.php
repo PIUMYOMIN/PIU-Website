a41e94915c9df65a71d866652347f51d3e03f44a
Myo Min Thu <myominthu819@gmail.com>
2024-04-22 11:03:02 +0630

    login api cors updated

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         'auth.api' => \App\Http\Middleware\AuthApiMiddleware::class,
+         \App\Http\Middleware\Cors::class,
     ];
 
     /**
