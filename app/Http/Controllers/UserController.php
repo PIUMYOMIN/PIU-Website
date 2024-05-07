9cfc30cbc9ad5926c4229da1a520d1c2baa16873
Myo Min Thu <myominthu819@gmail.com>
2024-05-07 16:03:59 +0630

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,8 +75,9 @@ class UserController extends Controller
     }
 
 
-    public function show(User $user)
+    public function show($id)
     {
+        $user = User::where('id', $id)->firstOrFail();
        return view('admin.user.show',[
         'user' => $user,
         'roles' => Role::all(),
