1b3b952e0a197409b784764f6aaa826bdb794696
Myo Min Thu <myominthu819@gmail.com>
2024-07-11 17:02:55 +0700

    updated

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,15 +18,15 @@ class Cors
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Accept,Authorization,Content-Type, X-Requested-With');
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Accept,Authorization,Content-Type');
 
         if ($request->isMethod('OPTIONS')) {
-            $response->headers->set('Access-Control-Allow-Origin', '*');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Accept,Authorization,Content-Type, X-Requested-With');
-            return $response;
+            return response()->json('', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Accept,Authorization,Content-Type');
         }
 
         return $response;
