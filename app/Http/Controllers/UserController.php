ddcf67172be2cba6eb1d2ef239990e91c708f651
Myo Min Thu <myominthu819@gmail.com>
2024-12-06 11:28:18 +0700

    google user login fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -383,7 +383,7 @@ public function user_login(Request $request)
 
     public function redirectToTwitter()
     {
-        dd('hit');
+        // dd('hit');
         return Socialite::driver('twitter')->redirect();
     }
 
