75ac577c70c0752561d5f31f4cf343e326617390
Myo Min Thu <myominthu819@gmail.com>
2026-04-08 19:26:31 +0400

    updated

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,24 +1,20 @@
 <?php
 
 namespace Database\Seeders;
-use Spatie\Permission\Models\Role;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        $role = Role::create(['name' => 'admin']);
-        $role = Role::create(['name' => 'manager']);
-        $role = Role::create(['name' => 'staff']);
-        $role = Role::create(['name' => 'registrar']);
-        $role = Role::create(['name' => 'teacher']);
-        $role = Role::create(['name' => 'student']);
-        $role = Role::create(['name' => 'user']);
+        $roles = ['admin', 'manager', 'staff', 'registrar', 'teacher', 'student', 'user'];
+
+        foreach ($roles as $name) {
+            Role::firstOrCreate(
+                ['name' => $name, 'guard_name' => 'web']
+            );
+        }
     }
 }
